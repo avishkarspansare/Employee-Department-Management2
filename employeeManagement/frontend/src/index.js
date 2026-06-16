2bfe7c203d208ac1be368fc2fe2f150951414d84
Avishkar Pansare <87558645+avishkarspansare@users.noreply.github.com>
2026-06-16 12:39:23 +0530

    Add Bootstrap and custom CSS imports to index.js

--- employeeManagement/frontend/src/index.js
+++ employeeManagement/frontend/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+serviceWorker.unregister();
